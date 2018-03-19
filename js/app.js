9b4cf3ab4260761cde582e28c8a121ac33453258
Maira Martins K <mairamartinsk@gmail.com>
2018-03-19 16:37:17 +0100

    Feature: randomly generate new cards

--- js/app.js
+++ js/app.js
@@ -1,38 +1,47 @@
-/*
- * Create a list that holds all of your cards
- */
+// Randomly add icons to cards
+function generateCards() {
+  // List of all different icons to display
+  const icon = [
+    "fa-diamond",
+    "fa-paper-plane-o",
+    "fa-anchor",
+    "fa-bolt",
+    "fa-cube",
+    "fa-leaf",
+    "fa-bicycle",
+    "fa-bomb"
+  ];
 
+  // Shuffle list of icons calling Shuffle function
+  let iconsOne = shuffle(icon);
+  let iconsTwo = shuffle(icon);
+  let randomIcons = iconsOne.concat(iconsTwo);
+  randomIcons = shuffle(randomIcons);
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+  // Loop through each card and generate its HTML
+  let cards = [...document.querySelectorAll(".card")];
+
+  for (let i = 0; i < cards.length; i++) {
+    const randomCard = document.createElement("i");
+    randomCard.setAttribute("class", "fa");
+    randomCard.classList.add(randomIcons[i]);
+    cards[i].appendChild(randomCard);
+  }
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
